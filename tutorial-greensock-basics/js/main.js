9027238a829eb703498c0d7ba0ad645c5b71a710
Eduardo Bonamigo Müller <edbmuller@gmail.com>
2020-09-10 00:23:57 -0300

    Exercize: animating multiple elements using timeline

--- tutorial-greensock-basics/js/main.js
+++ tutorial-greensock-basics/js/main.js
@@ -18,17 +18,49 @@
 //   }
 // );
 
-gsap.fromTo('header', 
-  {x: -40}, 
-  {
-    x:40, 
-    duration: .5,
-    repeat: -1,
-    ease: 'power2.inOut',
-    yoyo: true,
-  }
-)
+// gsap.fromTo('header', 
+//   {x: -40}, 
+//   {
+//     x:40, 
+//     duration: .5,
+//     repeat: -1,
+//     ease: 'power2.inOut',
+//     yoyo: true,
+//   }
+// )
 
-gsap.set( 'ul',
-  { y: 40 }
+// gsap.set( 'ul',
+//   { y: 40 }
+// )
+
+// Exercize:
+let tl = gsap.timeline({
+  duration: .4,
+  ease: 'power1.out',
+})
+
+tl.fromTo( 'body', 
+  { backgroundColor: '#fff' },
+  {
+    duration: .8,
+    backgroundColor: '#76c897',
+    ease: 'linear',
+  }
+).from( ['h1', 'p'],
+  { 
+    opacity: 0, 
+    y: -20,
+    stagger: .4,
+  }
+).from( 'img, h2',
+  { 
+    opacity: 0 
+  }
+).from( 'ul li',
+  {
+    zIndex: -1,
+    opacity: 0,
+    y: -20,
+    stagger: .2,
+  }
 )
