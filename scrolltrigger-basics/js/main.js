0f1a3187847987032ef715b58764b3d64edef884
Eduardo Bonamigo Müller <edbmuller@gmail.com>
2020-09-10 20:08:24 -0300

    toggleClass with scrollTrigger

--- scrolltrigger-basics/js/main.js
+++ scrolltrigger-basics/js/main.js
@@ -2,15 +2,29 @@ function init() {
 
   gsap.registerPlugin(ScrollTrigger);
 
-  gsap.to('#intro img', {
+  // Simple tween
+/*  gsap.to('#intro img', {
     scrollTrigger: {
       trigger: '#intro',
       start: 'top top',
       end: 'bottom center',
       scrub: true,
-      markers: true
+      // markers: true
     },
     opacity: 0, 
+  });
+*/
+
+  // Toogle css class
+  gsap.set('#project02', {
+    scrollTrigger: {
+      trigger: '#project02',
+      start: 'top bottom+=10%',
+      end: 'bottom center-=10%',
+      toggleClass: 'active',
+      markers: true,
+      reverse: true,
+    }
   });
 
 }
