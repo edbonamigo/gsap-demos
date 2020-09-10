14d5ac2fd8173e2f1af0e948dc2df956a88d43ae
Eduardo Bonamigo Müller <edbmuller@gmail.com>
2020-09-09 23:54:30 -0300

    easing, .from, stagger, repeat"

--- tutorial-greensock-basics/js/main.js
+++ tutorial-greensock-basics/js/main.js
@@ -1,8 +1,19 @@
-gsap.to(
-  'li',
-  { 
-    duration: .4,
-    y: 20,
-    // ease: power.out
-  },
+gsap.from( 'ul li:not(:last-child)', {
+  duration: .4,
+   x: -10,
+   ease: 'power1.out',
+   opacity: 0,
+   stagger: .1,
+  //  repeat: 1,
+  //  repeatDelay: 1,
+  }
+);
+
+gsap.to( 'ul li:last-child', {
+  duration: .4,
+   x: 50,
+   ease: 'power2.out',
+   delay: .7,
+   yoyo: true,
+  }
 );
