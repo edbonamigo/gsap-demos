0226f00864ab6f9cef8ce4283d25f508081a5cba
Eduardo Bonamigo Müller <edbmuller@gmail.com>
2020-09-10 19:25:36 -0300

    Plugin register and scrollTrigger usage

--- scrolltrigger-basics/js/main.js
+++ scrolltrigger-basics/js/main.js
@@ -1,7 +1,20 @@
-function init(){
-    
+function init() {
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to('#intro img', {
+    scrollTrigger: {
+      trigger: '#intro',
+      start: 'top top',
+      end: 'bottom center',
+      scrub: true,
+      markers: true
+    },
+    opacity: 0, 
+  });
+
 }
 
-window.addEventListener('load', function(){
-    init();
-});
+window.addEventListener('load', function() {
+  init();
+})
