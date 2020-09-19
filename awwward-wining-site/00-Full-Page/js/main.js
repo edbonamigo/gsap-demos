9ff1f3fa9560e6adad87e54c95830c24ebb20b52
Eduardo Bonamigo Müller <edbmuller@gmail.com>
2020-09-19 15:26:10 -0300

    main nav  animations

--- awwward-wining-site/00-Full-Page/js/main.js
+++ awwward-wining-site/00-Full-Page/js/main.js
@@ -2,8 +2,39 @@ gsap.registerPlugin(ScrollTrigger);
 
 function init(){
     
-    // start here
+  const mainNavLinks = gsap.utils.toArray('.main-nav a');
+  const mainNavLinksReversed = gsap.utils.toArray('.main-nav a').reverse();
+  mainNavLinks.forEach( link => {
+    link.addEventListener('mouseleave', e => {
+      link.classList.add('animate-out');
+      setTimeout(() => {
+        link.classList.remove('animate-out');
+      }, 300)
+    });
+  });
 
+  function navAnimation(direction) {
+    const scrollDown = direction === 1;
+    const links = scrollDown ? mainNavLinks : mainNavLinksReversed;
+    return gsap.to( links, {
+      duration: 0.3,
+      stagger: 0.05,
+      autoAlpha: () => scrollDown ? 0 : 1,
+      y: () => scrollDown ? 20 : 0,
+      ease: 'Power4.out',
+    });
+  }
+
+  ScrollTrigger.create({
+    start: 100,
+    end: 'bottom bottom-=20',
+    toggleClass: {
+      targets: 'body',
+      className: 'has-scrolled'
+    },
+    onEnter: ({direction}) => navAnimation(direction),
+    onLeaveBack: ({direction}) => navAnimation(direction)
+  });
 }
 
 window.addEventListener('load', function(){
